3b2d8bc43f37fe44fa6843304baccc8d8647a630
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-10-02 13:57:05 -0400

    testing out fortune cookie home page

--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,14 @@ class Home extends Component {
 
     constructor(){
         super()
-        this.state = {displayApp: false}
+        this.state = {
+            displayApp: false, 
+            displayedText: "Have A Nice Day"
+        }
     }
 
     componentDidMount = () => {
-        this.start()
+        // this.start()
     }
 
     start() {
@@ -41,6 +44,24 @@ class Home extends Component {
         })
     }
 
+    changeText = (num) => {
+        switch(num) {
+            case 1:
+                return this.setState({ displayedText: "You Are Very Talented In Many Ways" })
+            case 2: 
+                return this.setState({ displayedText: "A Dream You Have Will Come True" })
+            case 3:
+                return this.setState({ displayedText: "Have A Nice Day" })
+            case 4:
+                return this.setState({ displayedText: "The Fortune You Seek Is In Another Cookie" })
+            case 5:
+                return this.setState({ displayedText: "Click To Enter" })
+            default:
+                return null
+
+        }
+    }
+
 
     render() {
         console.log('Thanks for the visit 😘')
@@ -49,14 +70,15 @@ class Home extends Component {
                 {this.state.displayApp ? <App /> : 
                 <div className="fixed top-0 bottom-0 left-0 right-0" onClick={this.loadApp}>
                     <div className="flex flex-column justify-center items-center home"  style={{marginTop: "20%"}}>
-                        <p className="yin" id="1">YINYANG</p>
-                        <p className="yin" id="2">YINYANG</p>
-                        <p className="yin yang" id="3">YINYANG</p>
-                        <p className="yin" id="4">YINYANG</p>
-                        <p className="yin" id="5">YINYANG</p>
+                        <p className="yin" id="1" onMouseEnter={this.changeText.bind(null, 1)}>YINYANG</p>
+                        <p className="yin" id="2" onMouseEnter={this.changeText.bind(null, 2)}>YINYANG</p>
+                        <p className="yin" id="3" onMouseEnter={this.changeText.bind(null, 3)}>YINYANG</p>
+                        <p className="yin" id="4" onMouseEnter={this.changeText.bind(null, 4)}>YINYANG</p>
+                        <p className="yin" id="5" onMouseEnter={this.changeText.bind(null, 5)}>YINYANG</p>
+                        <p style={{fontFamily: "Arial Black"}}>{this.state.displayedText}</p>
                     </div>
-                        <p className="center ml3" style={{fontFamily: "Arial Black"}}>Have A Nice Day</p>
                     
+                  
                 </div>}
             </div>
         );
